e0f6d55e430baf7961c7c9b4f9c6e2ea1c9e9be2
Mrdadgoku <dr084891@gmail.com>
2024-06-01 08:18:05 +0530

    Update userbot.py

--- DAXXMUSIC/core/userbot.py
+++ DAXXMUSIC/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="DAXXAss1",
+            name="NezukoxAssistant_bot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="DAXXAss2",
+            name="NezukoxAssistant_bot2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="DAXXAss3",
+            name="NezukoxAssistant_bot3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="DAXXAss4",
+            name="NezukoxAssistant_bot4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="DAXXAss5",
+            name="NezukoxAssistant_bot5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("The_friendz")
+                await self.one.join_chat("GOKUxEDITION")
               
-                await self.one.join_chat("RoY_editx")
+                await self.one.join_chat("BEST_FRIEND_FOREVER1230")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("The_friendz")
+                await self.two.join_chat("GOKUxEDITION")
                 
-                await self.one.join_chat("RoY_editx")
+                await self.one.join_chat("BEST_FRIEND_FOREVER1230")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("The_friendz")
+                await self.three.join_chat("GOKUxEDITION")
                 
-                await self.one.join_chat("RoY_editx")
+                await self.one.join_chat("BEST_FRIEND_FOREVER1230")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("The_friendz")
+                await self.four.join_chat("GOKUxEDITION")
                 
-                await self.one.join_chat("roy_editx")
+                await self.one.join_chat("BEST_FRIEND_FOREVER1230")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("The_friendz")
+                await self.five.join_chat("GOKUxEDITION")
                 
-                await self.one.join_chat("roy_editx")
+                await self.one.join_chat("BEST_FRIEND_FOREVER1230")
             except:
                 pass
             assistants.append(5)
